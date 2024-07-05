4e3ac886091a5333c5d995001d37d736d34087a6
OdhiamboMoses <odhiambomoses08@gmail.com>
2024-07-05 09:12:29 +0300

    input validation

--- index.js
+++ index.js
@@ -12,17 +12,24 @@ const massContainer = document.getElementById("mass-container");
 
 subBtn.addEventListener("click", function () {
   let unitToConvert = Number(userInput.value);
-  let lengthValue = (unitToConvert * 3.281).toFixed(3);
-  let volumeValue = (unitToConvert * 0.268).toFixed(3);
-  let massValue = (unitToConvert * 2.204).toFixed(3);
+  if (unitToConvert >= 0) {
+    let lengthValue = (unitToConvert * 3.281).toFixed(3);
+    let volumeValue = (unitToConvert * 0.268).toFixed(3);
+    let massValue = (unitToConvert * 2.204).toFixed(3);
 
-  lengthContainer.textContent = `${unitToConvert} meters = ${lengthValue} feet | ${unitToConvert} feet = ${(
-    unitToConvert / 3.281
-  ).toFixed(3)} meters`;
-  volumeContainer.textContent = `${unitToConvert} litres = ${volumeValue} gallons | ${unitToConvert} gallons = ${(
-    unitToConvert / 0.264
-  ).toFixed(3)} litres`;
-  massContainer.textContent = `${unitToConvert} kilos = ${massValue} pounds | ${unitToConvert} pounds = ${Number(
-    unitToConvert / 2.204
-  ).toFixed(3)} kilos`;
+    lengthContainer.textContent = `${unitToConvert} meters = ${lengthValue} feet | ${unitToConvert} feet = ${(
+      unitToConvert / 3.281
+    ).toFixed(3)} meters`;
+    volumeContainer.textContent = `${unitToConvert} litres = ${volumeValue} gallons | ${unitToConvert} gallons = ${(
+      unitToConvert / 0.264
+    ).toFixed(3)} litres`;
+    massContainer.textContent = `${unitToConvert} kilos = ${massValue} pounds | ${unitToConvert} pounds = ${Number(
+      unitToConvert / 2.204
+    ).toFixed(3)} kilos`;
+  } else {
+    lengthContainer.textContent = "";
+    volumeContainer.textContent = "";
+    massContainer.textContent = "";
+    alert("Enter a value greater than 0");
+  }
 });
